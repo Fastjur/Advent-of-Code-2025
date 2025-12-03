20670e6cdb03c4572358f15f99e36b422690a738
Jurriaan Den Toonder <1493561+Fastjur@users.noreply.github.com>
2025-12-03 16:03:29 +0100

    Finish day 3

---
+++ src/days/day03/index.ts
@@ -0,0 +1,51 @@
+/**
+ * Advent of Code 2025 - Day 3
+ */
+
+import { max, parseLines, parseNumbers, sum } from '../../lib/index.js';
+
+function getLargestJoltageOfBatteryBank(batteryBank: number[], remainingNumbers: number): number {
+  if (remainingNumbers <= 0) {
+    return 0;
+  }
+  console.debug(`Bank (${remainingNumbers} remaining)`, batteryBank);
+  const validBank = batteryBank.slice(0, remainingNumbers > 1 ? -(remainingNumbers - 1) : undefined);
+  console.debug('Valid bank to look for highest number', validBank);
+  const highestNumber = max(validBank);
+  const highestNumberIdx = batteryBank.findIndex((value) => value === highestNumber);
+  console.debug('Highest number', highestNumber, 'index', highestNumberIdx);
+
+  const remainingBank = batteryBank.slice(highestNumberIdx + 1);
+  console.debug('Remaining bank', remainingBank);
+  return (
+    highestNumber * 10 ** (remainingNumbers - 1) + getLargestJoltageOfBatteryBank(remainingBank, remainingNumbers - 1)
+  );
+}
+
+export function part1(input: string): number {
+  const lines = parseLines(input);
+  const batteryBanks = lines.map((line) => parseNumbers(line, ''));
+
+  const joltages = batteryBanks.map((bank) => {
+    console.debug('\n\nChecking battery bank', bank);
+    return getLargestJoltageOfBatteryBank(bank, 2);
+  });
+  const summed = sum(joltages);
+  console.debug('Joltages', joltages);
+
+  return summed;
+}
+
+export function part2(input: string): number {
+  const lines = parseLines(input);
+  const batteryBanks = lines.map((line) => parseNumbers(line, ''));
+
+  const joltages = batteryBanks.map((bank) => {
+    console.debug('\n\nChecking battery bank', bank);
+    return getLargestJoltageOfBatteryBank(bank, 12);
+  });
+  const summed = sum(joltages);
+  console.debug('Joltages', joltages);
+
+  return summed;
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,27 @@
 /**
+ * Parse a string into an array of lines
+ */
+export function parseLines(input: string): string[] {
+  return input.trim().split('\n');
+}
+
+/**
+ * Parse a string into an array of numbers
+ */
+export function parseNumbers(input: string, separator: string | RegExp = /\s+/): number[] {
+  return input.trim().split(separator).map(Number);
+}
+
+/**
  * Sum an array of numbers
  */
 export function sum(numbers: number[]): number {
   return numbers.reduce((a, b) => a + b, 0);
 }
 
+/**
+ * Find the maximum value in an array
+ */
+export function max(numbers: number[]): number {
+  return Math.max(...numbers);
+}
